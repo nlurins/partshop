b8d6c42cd8920f033056fb4c325548ef22e2633a
n.lurins <lurins.normunds@gmail.com>
2022-08-01 12:23:15 +0300

    add static shopping cart layout

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3df59a4f80aca63e"; }
+/******/ 		__webpack_require__.h = function() { return "1529a465c3aeb03c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,37 @@
 import { BsCart3 } from 'react-icons/bs'
 import "@fontsource/montserrat";
 import Link from 'next/link';
+import {AiOutlineCloseSquare} from 'react-icons/ai';
+import { useState } from 'react';
 
 export const Header = () => {
+  const [active, setActive] = useState('hidden');
+  
   return (
     <div>
+       <div className= {`${active} font-["Montserrat"] text-6xl text-black font-bold  `}>
+         <div className='fixed min-h-[100vh] min-w-[100%] bg-black opacity-40'>
+          </div>
+          <div className='fixed min-h-[100vh] w-[30%] bg-white right-0 border shadow-xl'>
+          <div className='container flex flex-col w-[90%] mx-auto pt-[5%]'>
+            <AiOutlineCloseSquare className='self-end mb-10 cursor-pointer' onClick={() => {
+              setActive('hidden')
+            }}/>
+            <h1 className='underline text-center'>Shopping Cart</h1>
+          </div>
+          </div>
+       </div>
       <div className="container flex max-w-[90%] pt-28 mx-auto justify-between ">
           <Link href="/"><a className="font-['Montserrat'] tracking-wider text-4xl text-white font-bold hover:text-blue-gray-200">CAR PARTS AND SERVICES</a></Link>
           <div className="flex font-['Montserrat']  text-3xl text-white gap-20">
               <Link href="/shop"><a className='hover:text-blue-gray-200'>Shop</a></Link>
               <a className='hover:text-blue-gray-200'>Contacts</a>
-              <BsCart3 className='h-[40px] hover:text-blue-gray-200'/>
+              <BsCart3 className='h-[40px] hover:text-blue-gray-200 cursor-pointer' onClick={() => {
+                setActive('')
+              }}/>
           </div>
       </div>
+     
     </div>
   )
 }
